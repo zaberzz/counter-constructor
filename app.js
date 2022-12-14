0ca3289ecc7c98ecae7589741676c5ce622a7388
Saba <saba.sadegh67@gmail.com>
2022-12-14 18:29:06 +0330

    add eventListener to bts

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ function Counter(element, value) {
   this.decreaseBtn = element.querySelector(".decrease");
   this.valueDOM = element.querySelector(".value");
   this.valueDOM.textContent = this.value;
+  this.increase = this.increase.bind(this);
+  this.decrease = this.decrease.bind(this);
+  this.reset = this.reset.bind(this);
+
+  this.increaseBtn.addEventListener("click", this.increase);
+  this.decreaseBtn.addEventListener("click", this.decrease);
+  this.resetBtn.addEventListener("click", this.reset);
 }
 
 Counter.prototype.increase = function () {
