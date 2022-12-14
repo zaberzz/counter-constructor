45fcbd464d71697a4c4849500c5bc99104ab1aea
Saba <saba.sadegh67@gmail.com>
2022-12-14 18:41:54 +0330

    add method to function

--- app-class.js
+++ app-class.js
@@ -15,22 +15,20 @@ class Counter {
     this.decreaseBtn.addEventListener("click", this.decrease);
     this.resetBtn.addEventListener("click", this.reset);
   }
+  increase() {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+  }
+
+  decrease() {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+  }
+  reset() {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+  }
 }
-
-Counter.prototype.increase = function () {
-  this.value++;
-  this.valueDOM.textContent = this.value;
-};
-
-Counter.prototype.decrease = function () {
-  this.value--;
-  this.valueDOM.textContent = this.value;
-};
-
-Counter.prototype.reset = function () {
-  this.value = 0;
-  this.valueDOM.textContent = this.value;
-};
 
 const firstCounter = new Counter(getElement(".first-counter"), 100);
 const secondCounter = new Counter(getElement(".second-counter"), 200);
