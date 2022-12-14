745245c23cb07d7e1cdb5065a7a012276d722a91
Saba <saba.sadegh67@gmail.com>
2022-12-14 18:23:16 +0330

    creating prototype functions

--- app.js
+++ app.js
@@ -8,23 +8,20 @@ function Counter(element, value) {
   this.valueDOM.textContent = this.value;
 }
 
-Counter.prototype.increase=function{
-  this.value++
+Counter.prototype.increase = function () {
+  this.value++;
   this.valueDOM.textContent = this.value;
+};
 
-}
-
-Counter.prototype.decrease=function{
-  this.value--
+Counter.prototype.decrease = function () {
+  this.value--;
   this.valueDOM.textContent = this.value;
+};
 
-}
-
-Counter.prototype.reset=function{
-  this.value=0
+Counter.prototype.reset = function () {
+  this.value = 0;
   this.valueDOM.textContent = this.value;
-
-}
+};
 
 const firstCounter = new Counter(getElement(".first-counter"), 100);
 const secondCounter = new Counter(getElement(".second-counter"), 200);
